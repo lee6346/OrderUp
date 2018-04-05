16fd0a422dcb9476243ba63d7d3c74cba7f31e81
James Lee <james.i.lee.00@gmail.com>
2018-04-05 18:58:41 -0500

    fixed config errs

--- app/server/server.js
+++ app/server/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
-const config = require('./config/config');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-//const path = require('path');
-mongoose.connect(config.mongo.localdb);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -24,37 +22,6 @@ app.use(passport.session());
 const PORT = process.env.PORT || 5000;
 
 require('./routes/authRoutes')(app);
-
-
-
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-
-//set static files middleware
-//app.use(express.static(__dirname + '/public'));
-
-
-
-//configure routes 
-
-
-
-
-app.get('/', (request, response) => {
-    response.render('pages/index');
-});
-
-app.get('/home', (request, response) => {
-    response.render('pages/index');
-});
-
-app.get('/about', (request, response) => {
-    response.render('pages/about');
-});
-
-app.get('/account/:id', (request, response) => {
-    response.send(`Hello ${request.params['id']}`);
-});
 
 
 
